e1f7c81485358ac6eee0f71504f402ebee5a24c0
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-02-28 12:57:40 +0530

    Started With Product and ProductCard Layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import { IoAccessibility } from "react-icons/io5";
 import './App.css'
+import Product from './Components/Produc';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-gray-700 flex items-center justify-center' >
-        <h1>Hello This is Jeevith</h1> 
-        <IoAccessibility/>
-      </div>
+       <div>
+            <Product/>
+       </div>
     </>
   )
 }
