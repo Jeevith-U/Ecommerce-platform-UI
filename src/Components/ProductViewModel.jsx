374a584f90cb66c8f5275f4ab073a6bf6869a75f
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-03-01 11:15:13 +0530

    Added the Status.jsx which gives info about status of availability

--- src/Components/ProductViewModel.jsx
+++ src/Components/ProductViewModel.jsx
@@ -2,47 +2,96 @@
 * All the Template for this is Imported fom a library called HeadlessUI 
 */
 
-import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Dialog, DialogTitle } from '@headlessui/react'
+import { Divider } from '@mui/material';
 import { useState } from 'react'
+import Status from './Status';
+import { MdClose, MdDone } from 'react-icons/md';
 
-const ProductViewModel = ({open, setOpen, product, isAvailable}) => {
-    
-    const{productId,productName,image,description,quantity,price,discount,specialPrice} = product ;
+const ProductViewModel = ({ open, setOpen, product, isAvailable }) => {
 
-    const handelClickOpen = () =>{
-        setOpen(true) ;
+    const { productId, productName, image, description, quantity, price, discount, specialPrice } = product;
+
+    const handleClose = () => {
+        setOpen(false);
     }
 
     return (
         <>
-  
-        <Dialog open={open} as="div" className="relative z-10 focus:outline-none" onClose={close}>
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4">
-              <DialogPanel
-                transition
-                className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-              >
-                <DialogTitle as="h3" className="text-base/7 font-medium text-slate-800">
-                  {productName}
-                </DialogTitle>
-                <p className="mt-2 text-sm/6 text-slate-800">
-                  {description}
-                </p>
-                <div className="mt-4">
-                  <Button
-                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
-                    onClick={() => setOpen(false)}
-                  >
-                    Got it, thanks!
-                  </Button>
+            <Dialog open={open} as="div" className="relative z-10" onClose={handleClose}>
+                <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4">
+                        <div className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all md:max-w-[620px] md:min-w-[620px] w-full">
+                            {image && (
+                                <div className='flex justify-center aspect-[3/2]'>
+                                    <img
+                                        className="w-full h-full cursor-pointer transition-transform"
+                                        src={image}
+                                        alt={productName} />
+                                </div>
+                            )}
+
+                            <div className='px-6 pt-10 pb-2'>
+                                <DialogTitle as="h1" className="lg:text-3xl sm:text-2xl text-xl font-semibold leading-6 text-gray-800 mb-4">
+                                    {productName}
+                                </DialogTitle>
+
+                                <div className='space-y-2 text-gray-700 pb-4'>
+                                    <div className='flex items-center justify-between gap-2'>
+                                        {specialPrice ? (
+                                            <div className="flex flex-col">
+                                                <span className="text-gray-400 line-through">
+                                                    ${Number(price).toFixed(2)}
+                                                </span>
+                                                <span className="text-xl font-bold text-slate-700">
+                                                    ${Number(specialPrice).toFixed(2)}
+                                                </span>
+                                            </div>
+                                        ) : (
+                                            <span className="text-xl font-bold text-slate-700">
+                                                {"  "}
+                                                ${Number(price).toFixed(2)}
+                                            </span>
+                                        )}
+
+                                        { isAvailable ? (
+                                            <Status
+                                                text= "Instock"
+                                                icon={MdDone}
+                                                bg="bg-teal-200"
+                                                color="text-teal-600"
+                                                />
+                                        ) :(
+                                            <Status
+                                            text= "Out-of-Stocks"
+                                             icon={MdClose}
+                                             bg="bg-teal-200"
+                                             color="text-teal-600"
+                                            />
+                                        )}
+                                    </div>
+
+                                    <Divider />
+                                    <p>{description}</p>
+                                </div>
+                            </div>
+
+                            <div className="flex justify-end p-4">
+                                <button 
+                                    onClick={handleClose}
+                                    type='button'
+                                    className='px-4 py-2 text-sm font-semibold text-slate-700 hover:text-slate-900 hover:border-slate-800 rounded-md'
+                                >    
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </DialogPanel>
-            </div>
-          </div>
-        </Dialog>
-      </>
+            </Dialog>
+        </>
     )
 }
 
-export default ProductViewModel
+export default ProductViewModel;
